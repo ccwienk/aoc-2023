af05828130b5654d668b37f2de5636b9e62f644a
Christian Cwienk <christian.cwienk@sap.com>
2023-12-10 10:20:31 +0100

    solve day-8a

--- 08/08.py
+++ 08/08.py
@@ -5,7 +5,40 @@ import util
 
 def main():
     with open(util.input_file) as f:
-        pass
+        directions = f.readline().strip()
+        f.readline()
+        node_lines = f.readlines()
+
+    def iter_directions():
+        # wrap directions forever
+        while True:
+            for c in directions:
+                yield c
+
+
+    nodes = {}
+
+    for line in node_lines:
+        node_id, remainder = line.strip().split(' = ')
+        remainder = remainder.strip('(').strip(')')
+        left, right = remainder.split(', ')
+
+        nodes[node_id] = (left, right)
+
+    node_id = 'AAA'
+    for step, direction in enumerate(iter_directions(), 0):
+        left, right = nodes[node_id]
+        if node_id == 'ZZZ':
+            print('done')
+            break
+        if direction == 'L':
+            node_id = left
+            left, right = nodes[left]
+        else:
+            node_id = right
+            left, right = nodes[right]
+
+    print(step)
 
 
 if __name__ == '__main__':

b394790efb4d270a5f0130fc21a71aae2ac97be7
Gardener CI/CD <christian.cwienk@sap.com>
2023-12-18 20:17:25 +0100

    solve day-15a

--- 15/15.py
+++ 15/15.py
@@ -3,9 +3,27 @@
 import util
 
 
+def crishash(s: str):
+    v = 0
+
+    for c in s:
+        v += ord(c)
+        v *= 17
+        v %= 256
+
+    return v
+
+
 def main():
     with open(util.input_file) as f:
-        pass
+        strs = f.read().strip().split(',')
+
+    total = 0
+
+    for s in strs:
+        total += crishash(s)
+
+    print(f'{total=}')
 
 
 if __name__ == '__main__':
